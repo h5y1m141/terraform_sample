e1dbb9a4e18cfcfa158096251296a4b5a1188547
HiroshiOyamada <h5y1m141@gmail.com>
2018-03-28 07:17:54 +0900

    public/privateなセグメントを設定するところまで対応

---
+++ README.md
@@ -0,0 +1,16 @@
+## このリポジトリについて
+
+Terraform使って、AWSの環境構築するサンプル
+
+
+## 目指す構成
+
+以下を目指して構成を作っていく
+
+- 参考にしてる情報を元にしてVPC使って、public/privateなセグメントが切られてる
+- 踏み台となるサーバー経由でアプリケーション・サーバーにSSHできる
+
+## 参考にしてる情報
+
+[お金をかけずに、TerraformでAWSのVPC環境を準備する](https://qiita.com/CkReal/items/1dbbc78888e157a80668)
+[TerraformでVPCを管理するmoduleを作る](https://www.sambaiz.net/article/121/)
---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  profile = "private-aws"
+  region = "ap-northeast-1"
+}
+module "dev-vpc" {
+  source = "./vpc"
+  vpc_name = "dev-vpc"
+  vpc_cidr_block = "10.0.0.0/16"
+  public_subnet_cidr_blocks = ["10.0.0.0/24", "10.0.1.0/24"]
+  public_subnet_availability_zones = ["ap-northeast-1a", "ap-northeast-1c"]
+  private_subnet_cidr_blocks = ["10.0.2.0/24", "10.0.3.0/24"]
+  private_subnet_availability_zones = ["ap-northeast-1a", "ap-northeast-1c"]
+}
